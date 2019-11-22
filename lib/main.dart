0c0e37d7295bf8d4e9ccf4dfe904b8135e96d658
Hasib Ahmed <hasibahmed1996@gmail.com>
2019-11-22 02:27:28 +0000

    redo of initial app, created nicer scaffold for further changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
-import 'pages/home.dart';
+import 'package:rethink_flutter_app/pages/setup/sign_in.dart';
 
-void main() {
-  runApp(new MaterialApp(home: new HomePage()));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ReThinkApp',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: LoginPage(),
+    );
+  }
 }
---
+++ lib/pages/setup/sign_in.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rethink_flutter_app/pages/home.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _email, _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(),
+      body: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                validator: (input) {
+                  if (input.isEmpty) {
+                    return 'Provide a valid email';
+                  }
+                },
+                decoration: InputDecoration(labelText: 'Email'),
+                onSaved: (input) => _email = input,
+              ),
+              TextFormField(
+                validator: (input) {
+                  if (input.length < 1) {
+                    return 'Provide a valid password';
+                  }
+                },
+                decoration: InputDecoration(labelText: 'Password'),
+                onSaved: (input) => _password = input,
+                obscureText: true,
+              ),
+              RaisedButton(
+                onPressed: signIn,
+                child: Text('Sign in'),
+              ),
+            ],
+          )),
+    );
+  }
+
+  void signIn() async {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      try {
+        FirebaseUser user = (await FirebaseAuth.instance
+                .signInWithEmailAndPassword(email: _email, password: _password))
+            .user;
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Home(user: user)));
+      } catch (e) {
+        print(e.toString());
+      }
+    }
+  }
+}
