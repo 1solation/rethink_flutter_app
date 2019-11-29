5537ecba78272f5218dd2d29b6c682d47a6597d9
Jamie Forster <J.Forster1@2017.ljmu.ac.uk>
2019-11-29 11:45:44 +0000

    Data driven boardroom jamie (#4)
    * Data Driven Development and iOS Settings & Dependencies Added/Updated.
    
    - Boardrooms View Created with Initial Styling
    - CocoaPods Added (iOS)
    - Signing and Capabilities updated (iOS & XCode)
    
    * Boardrooms and Polls List Styled and Refactored
    
    - Boardrooms List Refactored and Styled
    - Boardroom Page Created
    - Polls List Refactored and Styled
    - Initial Poll Page Created
    
    * Requested Review Changes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,43 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:rethink_flutter_app/pages/test_poll_list.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rethink_flutter_app/pages/poll_list.dart';
+
 
 class Home extends StatefulWidget {
   const Home({Key key, @required this.user}) : super(key: key);
-
+  // TODO: Global Styling Options
   final FirebaseUser user;
 
   @override
   _HomeState createState() => _HomeState();
 }
 
-Widget _buildListItem(BuildContext, context, DocumentSnapshot document) {
-  return ListTile(
-    title: Row(
-      children: [
-        Expanded(
-            child: Text(
-          document.documentID,
-          style: Theme.of(context).textTheme.headline,
-        )),
-        Container(
-          decoration: const BoxDecoration(
-            color: Color(0xffddddff),
-          ),
-          padding: const EdgeInsets.all(10.0),
-          child: Text(
-            document['userType'].toString(),
-            style: Theme.of(context).textTheme.display1,
-          ),
-        ),
-      ],
-    ),
-    onTap: () {
-      print("Should increase votes here.");
-    },
+
+Widget _buildBoardroomList(context, DocumentSnapshot snapshot, FirebaseUser user) {
+  //TODO: Dynamic Styling
+
+  return Card(
+      color: Colors.pink,
+      child: Column(
+          children: <Widget>[
+            ListTile(
+                leading: Icon(Icons.local_hospital, size: 50, color: Colors.white),
+                title: Text(snapshot["boardName"], style: new TextStyle(color: Colors.white)),
+                subtitle: Text(snapshot.documentID, style: new TextStyle(color: Colors.white70)),
+                onTap: (){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Boardroom(user: user, document: snapshot),
+                      )
+                  );
+                }
+            ),
+          ]
+      )
   );
 }
+
+
 
 class _HomeState extends State<Home> {
   @override
@@ -46,27 +48,69 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text('Home ${widget.user.email}'),
       ),
-      body: StreamBuilder(
-          stream: Firestore.instance
-              .collection('users')
-              .document('${widget.user.uid}'.toString())
-              .collection('activeBoards')
-              .snapshots(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) return const Text('Loading....');
-            print(snapshot.data);
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: StreamBuilder(
+            stream: Firestore.instance.collection('boardroom').where('members', arrayContains: widget.user.uid).snapshots(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (!snapshot.hasData)
+                return const Text('Loading....');
+
+              return ListView.builder(
+                itemExtent: 80.0,
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index) => _buildBoardroomList(context, snapshot.data.documents[index], widget.user),
+              );
+            }
+        )
+      )
+    );
+  }
+}
+
+class Boardroom extends StatefulWidget {
+  const Boardroom({Key key, @required this.user, this.document}) : super(key: key);
+
+  final FirebaseUser user;
+  final DocumentSnapshot document;
+
+  @override
+  _BoardroomState createState() => _BoardroomState();
+}
+
+class _BoardroomState extends State<Boardroom> {
+
+  //TODO: Redo theme related code to be dynamic, not hard-coded.
+
+  @override
+  Widget build(BuildContext context){
+    return _createList(context);
+  }
+
+  Scaffold _createList(BuildContext context){
+
+    return Scaffold(appBar: AppBar(
+      title: Text(widget.document['boardName']),
+    ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: StreamBuilder(
+          stream: Firestore.instance.collection('boardroom').document(widget.document.documentID).collection('polls').snapshots(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if(!snapshot.hasData){
+              return Text("Loading...");
+            } else {
+              print("[DEBUG] Has Data, Length: "+snapshot.data.documents.length.toString());
+            }
+
             return ListView.builder(
               itemExtent: 80.0,
               itemCount: snapshot.data.documents.length,
-              itemBuilder: (context, index) => _buildListItem(
-                  BuildContext, context, snapshot.data.documents[index]),
+              itemBuilder: (context, index) => buildPollList(context, snapshot.data.documents[index], widget.user),
             );
-          }),
+          },
+      ),
+      )
     );
-  }
-
-  void navigateToTestPolls() {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => PollListView()));
   }
 }
